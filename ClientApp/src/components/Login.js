4bc729591c200ce563e94b697cf06f5cf7028189
daniel <daniel_7866@yahoo.com>
2021-08-18 19:36:24 +0300

    Added Popup window with login form on the home page in react

---
+++ ClientApp/src/components/Login.js
@@ -0,0 +1,18 @@
+﻿import React from "react";
+
+const Login = () => {
+    return (
+        <form className="login-form">
+            <h2>Login</h2>
+            <label >Email:</label>
+            <input type="text" placeholder="Enter Email Address" />
+            <br/>
+            <label >Password:</label>
+            <input type="password" placeholder="Password" />
+            <br />
+            <button type="submit" class="btn btn-primary mb-3" onClick={(e) => { e.preventDefault(); }}>Login</button>
+        </form>
+        );
+};
+
+export default Login;
---
+++ ClientApp/src/components/Popup.js
@@ -0,0 +1,15 @@
+﻿import React from "react";
+import '../Styles/Popup.css';
+
+const Popup = (props) => {
+    return props.trigger ? (
+        <div className="popup">
+            <div className="popup-inner">
+                <button className="close-btn">X</button>
+                {props.children}
+            </div>
+        </div>
+    ): null;
+};
+
+export default Popup;
