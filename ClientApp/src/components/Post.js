ba81a88f8a2ddfb496c07076ea93f5d3f0258302
daniel <daniel_7866@yahoo.com>
2021-08-25 09:11:44 +0300

    Post component added to react.js

---
+++ ClientApp/src/components/Post.js
@@ -0,0 +1,15 @@
+﻿import React from "react";
+import "../Styles/Post.css";
+
+const Post = (props) => {
+
+    return (
+        <div className="post-container">
+            <h3><a>map from uid={props.userId} to username</a></h3>
+            <img className="post-image" src={props.imagePath} width="300" height="300" />
+            <p>{props.description}</p>
+        </div>
+    );
+};
+
+export default Post;
