70bd0d15a3ccf7b719c6558f22534c225b6fa2eb
daniel <daniel_7866@yahoo.com>
2021-08-26 15:47:44 +0300

    -Fixed css layout by using css grid to user profile. -Post component image size changed.

--- ClientApp/src/components/Profile.js
+++ ClientApp/src/components/Profile.js
@@ -18,26 +18,27 @@ const Profile = (props) => {
 
     return (
         <div className="profile-container">
-            <div className="image-cropper medium">
-                <img className="profile-image" src={props.imagePath} />
+            <div className="profile-container-top">
+                <div className="image-cropper small">
+                    <img className="profile-image" src={props.imagePath} />
+                </div>
+                <h3>{props.name}</h3>
             </div>
-            <h1>{props.name}</h1>
-            <div className="profile-container-secondary">
-                <div className="profile-people-container">
-                    <div className="profile-followers-list">
-                        <h3>Following:</h3>
-                        {props.followers.map(f =>
-                            (<ProfileListItem key={f.id} id={f.id} name={f.name} imagePath={f.imagePath} />))}
-                    </div>
-                    <div className="profile-following-list">
-                        <h3>Followers:</h3>
-                        {props.followers.map(f =>
-                            (<ProfileListItem key={f.id} id={f.id} name={f.name} imagePath={f.imagePath} />))}
-                    </div>
+            <div className="profile-people-container">
+                <div className="profile-follow-list">
+                    <h3>Following:</h3>
+                    {props.followers.map(f =>
+                        (<ProfileListItem key={f.id} id={f.id} name={f.name} imagePath={f.imagePath} />))}
                 </div>
-                <div className="profile-post-container">
-                    {props.posts.map(p => (<Post key={p.id} id={p.id} userId={p.userId} description={p.description} imagePath={p.imagePath} />))}
+                <div className="profile-follow-list">
+                    <h3>Followers:</h3>
+                    {props.followers.map(f =>
+                        (<ProfileListItem key={f.id} id={f.id} name={f.name} imagePath={f.imagePath} />))}
                 </div>
+            </div>
+            <div className="profile-post-container">
+                {props.posts.map(p => (<Post key={p.id} id={p.id} userId={p.userId} description={p.description}
+                    imagePath={p.imagePath} />))}
             </div>
         </div>
     )
