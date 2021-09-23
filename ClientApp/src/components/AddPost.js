619659a22ef4e7fef9b6b9a463b2a27656406577
Avtikh <avatikh@gmail.com>
2021-09-23 19:27:17 +0300

    added image storage

--- ClientApp/src/components/AddPost.js
+++ ClientApp/src/components/AddPost.js
@@ -1,18 +1,44 @@
 ﻿import React, { useState } from 'react';
 import { getAddress } from '../Services';
 import "../Styles/Post.css";
+import { storage } from "../firebase/index.js";
 
 const AddPost = (props) => {
     const [text, setText] = useState('');
-    const [file, setFile] = useState('');
+    const [image, setImage] = useState(null);
+
+    const handleChange = e => {
+        if (e.target.files[0]) {
+            setImage(e.target.files[0]);
+        }
+    };
 
     const uploadHandler = () => {
         var myHeaders = new Headers();
+        var imageURL;
         myHeaders.append("Content-Type", "application/json");
+
+        const uploadTask = storage.ref(`images/${image.name}`).put(image);
+        uploadTask.on(
+            "state_changed",
+            snapshot => { },
+            error => {
+                console.log(error);
+            },
+            () => {
+                storage
+                    .ref("images")
+                    .child(image.name)
+                    .getDownloadURL()
+                    .then(url => {
+                        imageURL=url;
+                    });
+            }
+        );
 
         var raw = JSON.stringify({
             "description": `${text}`,
-            "imagePath": ``
+            "imagePath": `${imageURL}`
         });
 
         var requestOptions = {
@@ -24,14 +50,14 @@ const AddPost = (props) => {
 
         fetch(`${getAddress()}/api/post/createpost`, requestOptions)
             .then(response => response.json())
-            .then(result => { console.log(result); props.setRefresh(value => !value); setText(""); setFile(""); })
+            .then(result => { console.log(result); props.setRefresh(value => !value); setText(""); setImage(null); })
             .catch(error => console.log('error', error));
     }
 
     return (
         <div className="add-post">
             <h3>Add a new post</h3>
-            <input type="file" value={file} onChange={e => setFile(e.target.value)} />
+            <input type="file" onChange={handleChange} />
             <input type="text" placeholder="Type description here" value={text} onChange={(e) => setText(e.target.value)} />
             <button className="btn btn-primary" onClick={() => { uploadHandler(); }}>Upload</button>
         </div>
---
+++ ClientApp/src/firebase/index.js
@@ -0,0 +1,17 @@
+﻿import firebase from "firebase/compat/app";
+import "firebase/compat/storage";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAgnWt531K5SGNbGwMQ-TNXU-gGBbwHRL8",
+    authDomain: "oop-project-5cde7.firebaseapp.com",
+    projectId: "oop-project-5cde7",
+    storageBucket: "oop-project-5cde7.appspot.com",
+    messagingSenderId: "395960131468",
+    appId: "1:395960131468:web:c4a965761d7815af3b00a9",
+    measurementId: "G-FRQ6RET28P"
+};
+
+firebase.initializeApp(firebaseConfig);
+const storage = firebase.storage();
+
+export { storage, firebase as default };
